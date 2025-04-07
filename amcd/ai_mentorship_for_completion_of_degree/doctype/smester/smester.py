87136671cb44f82a12e8c0e35b6ed65d5301081a
Raja Abdullah Arshad <rajaabdullaharsahd>
2025-04-08 00:40:07 +0500

    cleanup

--- amcd/ai_mentorship_for_completion_of_degree/doctype/smester/smester.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Nextash and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Smester(Document):
-	pass
