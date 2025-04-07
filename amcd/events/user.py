e4b0b1e189771db9104ab2e2242e18ff49d439f8
Raja Abdullah Arshad <rajaabdullaharsahd>
2025-04-07 23:19:11 +0500

    FIXES

---
+++ amcd/events/user.py
@@ -0,0 +1,18 @@
+import frappe
+
+def validate(doc, method):
+    amcd_settings = frappe.get_single("AMCD Settings")
+    organization_domain = amcd_settings.get("organization_domain")
+    
+    if not organization_domain:
+        frappe.throw("Organization domain is not set in AMCD Settings")
+    
+    if not doc.email:
+        frappe.throw("Email is required")
+
+    if "@" in doc.email:
+        email_domain = doc.email.split("@")[1]
+        if email_domain != organization_domain:
+            frappe.throw(f"Email domain must match the organization's domain: {organization_domain}")
+    else:
+        frappe.throw("Invalid email format")
